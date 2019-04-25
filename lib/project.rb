a7572dcb04ce979d415b1f7287ea323e364fb822
Karen V <karenvo@usc.edu>
2019-04-25 05:51:45 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,4 +4,8 @@ class Project
       @title = title
       @backers = []
     end
+    def add_backer(backer)
+      #accepts backer as an argument and stores it in backer array
+      @backers << new.backer
+    end
 end
