11f080bfba40c86d5f6f58183796c338cbce58ea
Karen V <karenvo@usc.edu>
2019-04-25 05:46:32 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,7 @@
 class Project
-    attr_reader :name, :backers
-    def initialize(name)
-      @name = name
+    attr_reader :title, :backers
+    def initialize(title)
+      @title = title
       @backers = []
     end
 end
